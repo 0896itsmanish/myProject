799342b44a29e1957b1a605b702d362797f79dea
manish.singh1 <manish.singh1@WKSBAN24KAN0022.corp.mphasis.com>
2019-10-26 09:51:13 +0530

    Manish Commit

---
+++ MyFirstGitHUB/src/Account.java
@@ -0,0 +1,24 @@
+
+public class Account {
+
+		int acno;
+		String acname;
+		public Account(int acno, String acname) {
+			super();
+			this.acno = acno;
+			this.acname = acname;
+		}
+		public int getAcno() {
+			return acno;
+		}
+		public void setAcno(int acno) {
+			this.acno = acno;
+		}
+		public String getAcname() {
+			return acname;
+		}
+		public void setAcname(String acname) {
+			this.acname = acname;
+		}
+		
+}
