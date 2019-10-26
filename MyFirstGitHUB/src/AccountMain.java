a1a1cbb08e20f0626551a74293f7bba7c97ec4d1
manish.singh1 <manish.singh1@WKSBAN24KAN0022.corp.mphasis.com>
2019-10-26 10:02:41 +0530

    added Main

---
+++ MyFirstGitHUB/src/AccountMain.java
@@ -0,0 +1,10 @@
+
+public class AccountMain {
+
+	public static void main(String[] args) {
+		Account user=new Account(121, "Manish");
+		System.out.println("ACCOUNT NUMBER:"+user.getAcno());
+		System.out.println("ACCOUNT NAME:"+user.getAcname());
+	}
+
+}
