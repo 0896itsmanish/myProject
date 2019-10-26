a087bdf2a4345af10a41db248b966e2f8c6f1248
0896itsmanish <57026105+0896itsmanish@users.noreply.github.com>
2019-10-26 10:05:37 +0530

    add a comment

--- MyFirstGitHUB/src/AccountMain.java
+++ MyFirstGitHUB/src/AccountMain.java
@@ -2,6 +2,7 @@
 public class AccountMain {
 
 	public static void main(String[] args) {
+		//etu toh moila project aseh dey
 		Account user=new Account(121, "Manish");
 		System.out.println("ACCOUNT NUMBER:"+user.getAcno());
 		System.out.println("ACCOUNT NAME:"+user.getAcname());
